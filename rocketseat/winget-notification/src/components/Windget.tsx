22e4c7d2e56bd8b91162e2f71a8ac2dc0524ff63
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2023-06-24 17:03:14 -0300

    Atualizado readme

--- rocketseat/winget-notification/src/components/Windget.tsx
+++ rocketseat/winget-notification/src/components/Windget.tsx
@@ -54,7 +54,7 @@ export function Windget() {
 
       {/* old section */}
       <div>
-        <div className="bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-500 dark:text-zinc-400">
+        <div className="font-medium bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-500 dark:text-zinc-400">
           Antigas
         </div>
 
