5128ee2a4f5d6a3c2e030599244b714191690088
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2023-07-24 07:47:05 -0300

    add font style

--- rocketseat/ignite/reactJS/shop/src/pages/_app.tsx
+++ rocketseat/ignite/reactJS/shop/src/pages/_app.tsx
@@ -1,5 +1,15 @@
+import { Roboto } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
