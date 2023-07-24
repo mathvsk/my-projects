c2befabfb6ceb983e65ddacddd337f4f10c7d7a2
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2023-07-24 08:57:30 -0300

    add lib stitches for css app configure stitches enbale when js web disable create a component create config for style

--- rocketseat/ignite/reactJS/shop/src/pages/_document.tsx
+++ rocketseat/ignite/reactJS/shop/src/pages/_document.tsx
@@ -1,9 +1,15 @@
+import { getCssText } from '@/styles'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }} // Isso aqui garante que o CSS seja renderizado no HTML mesmo desabilitando os JS da pagina
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- rocketseat/ignite/reactJS/shop/src/pages/index.tsx
+++ rocketseat/ignite/reactJS/shop/src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { styled } from '@/styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+})
+
 export default function Home() {
   return (
     <>
-      <h1>Hellow</h1>
+      <Button>Hellow</Button>
     </>
   )
 }
