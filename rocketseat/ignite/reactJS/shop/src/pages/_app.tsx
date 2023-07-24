caa572ccc345cf602482938bfbdb26f98fddc10b
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2023-07-24 09:45:54 -0300

    Added Image component from Next.js to adopt performance practices.

--- rocketseat/ignite/reactJS/shop/src/pages/_app.tsx
+++ rocketseat/ignite/reactJS/shop/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { globalStyles } from '@/styles/global'
 
 import logo from '@/assets/logo.svg'
 import { Container, Header } from '@/styles/pages/app'
+import Image from 'next/image'
 
 const roboto = Roboto({
   weight: '400',
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
+        <Image src={logo} alt="logo" />
         <img src={logo.src} />
       </Header>
 
