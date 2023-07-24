0f58e736aef0cc08c878e9c58ec83a750a23ef63
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2023-07-24 09:26:37 -0300

    create header of application create global styles create variables of style

--- rocketseat/ignite/reactJS/shop/src/pages/_app.tsx
+++ rocketseat/ignite/reactJS/shop/src/pages/_app.tsx
@@ -1,15 +1,27 @@
 import { Roboto } from 'next/font/google'
 import type { AppProps } from 'next/app'
+import { globalStyles } from '@/styles/global'
+
+import logo from '@/assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app'
 
 const roboto = Roboto({
   weight: '400',
   subsets: ['latin'],
 })
 
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
-      <Component {...pageProps} />
-    </main>
+    <Container>
+      <Header>
+        <img src={logo.src} />
+      </Header>
+
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
+    </Container>
   )
 }
--- rocketseat/ignite/reactJS/shop/src/pages/index.tsx
+++ rocketseat/ignite/reactJS/shop/src/pages/index.tsx
@@ -1,13 +1,3 @@
-import { styled } from '@/styles'
-
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-})
-
 export default function Home() {
-  return (
-    <>
-      <Button>Hellow</Button>
-    </>
-  )
+  return <></>
 }
