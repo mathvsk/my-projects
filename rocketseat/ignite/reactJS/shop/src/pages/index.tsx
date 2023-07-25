bba69669eb6576fd001293b1265f7ca9d66c2a2a
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2023-07-24 21:32:48 -0300

    add carousel using keen-slider

--- rocketseat/ignite/reactJS/shop/src/pages/index.tsx
+++ rocketseat/ignite/reactJS/shop/src/pages/index.tsx
@@ -4,10 +4,20 @@ import Image from 'next/image'
 import camisa1 from '@/assets/Type6.png'
 import camisa2 from '@/assets/2_explorer-t-shirt 1.png'
 
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2,
+      spacing: 48,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camisa1} width={520} height={480} alt="" />
 
         <footer>
@@ -15,7 +25,16 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
         <Image src={camisa2} width={520} height={480} alt="" />
 
         <footer>
--- rocketseat/ignite/reactJS/shop/src/styles/pages/home.ts
+++ rocketseat/ignite/reactJS/shop/src/styles/pages/home.ts
@@ -2,7 +2,6 @@ import { styled } from '@stitches/react'
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
   maxWidth: 'calc(100vw - (( 100vw - 1180px) / 2))',
   marginLeft: 'auto',
   minHeight: 656,
@@ -11,7 +10,6 @@ export const HomeContainer = styled('main', {
 export const Product = styled('div', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
-  padding: '.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',
