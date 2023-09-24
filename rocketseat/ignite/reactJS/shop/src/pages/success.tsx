348076337f6cef9cdcec106e3b5201daef2988a7
Matheus Viscki <matheusviscki@gmail.com>
2023-09-24 12:00:27 -0300

    create a getServerSideProps function to retrieve purchase and user data to be used on the page.

--- rocketseat/ignite/reactJS/shop/src/pages/success.tsx
+++ rocketseat/ignite/reactJS/shop/src/pages/success.tsx
@@ -1,20 +1,54 @@
+import { stripe } from '@/lib/stripe'
 import { ImageContainer, SuccessContainer } from '@/styles/pages/success'
+import { GetServerSideProps } from 'next'
+import Image from 'next/image'
 import Link from 'next/link'
+import Stripe from 'stripe'
 
-export default function Success() {
+interface ProductProps {
+  costumerName: string
+  product: {
+    name: string
+    imageUrl: string
+  }
+}
+
+export default function Success({ costumerName, product }: ProductProps) {
   return (
     <SuccessContainer>
       <h1>Compra Efetuada</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
-        casa.
+        Uhuul <strong>{costumerName}</strong>, sua
+        <strong> {product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href="/">Voltar ao catálago</Link>
     </SuccessContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = query.session_id as string
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  const customerName = session.customer_details?.name
+  const product = session.line_items?.data[0].price?.product as Stripe.Product
+
+  return {
+    props: {
+      costumerName: customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  }
 }
